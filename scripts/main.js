ab2807ff2f7d1a4f82eb94781b63997c643bed8a
chxrlxttxx <59923373+chxrlxttxx@users.noreply.github.com>
2020-05-12 14:57:15 +0200

    summary

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function geklickt (name) {
+    alert ("hello, who are you");
+    document.write("Hallo " + name);
+  }
+  geklickt ("User");
+
+function liebestest(name1, name2){
+    var punkte;
+    punkte = (((name1 + name2).length)/name2.length)*2;
+    document.write(name1 + " und " + name2 + " passen zu " + punkte*10 + "% zusammen!");
+  }
+  liebestest("Karin","Hans");
