ab80307ae09ebcad92dbe4dcded4c3a5a2cebdd0
chxrlxttxx <59923373+chxrlxttxx@users.noreply.github.com>
2020-05-14 11:49:18 +0200

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
 function geklickt (name) {
-    alert ("hello, who are you");
+    alert ("fa fa-fw fa-home Benachrichtigung");
     document.write("Hallo " + name);
   }
 
