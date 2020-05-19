a502db08df46801b574b879e9f86717016da16f6
chxrlxttxx <59923373+chxrlxttxx@users.noreply.github.com>
2020-05-19 14:24:53 +0200

    summary

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-function geklickt (name) {
-    alert ("fa fa-fw fa-home Benachrichtigung");
-    document.write("Hallo " + name);
-  }
+var game = document.querySelector("#game");
+game.style.color = "black";
+
+function geklickt () {
+alert (Date());
+} 
 
 function liebestest(name1, name2){
-    var punkte;
-    punkte = (((name1 + name2).length)/name2.length)*2;
-    document.write(name1 + " und " + name2 + " passen zu " + punkte*10 + "% zusammen!");
-  }
-  liebestest("Karin","Hans");
+  blur ()
+  var punkte;
+  punkte = (((name1 + name2).length)/name2.length)*2;
+  document.write(name1 + " und " + name2 + " passen zu " + punkte*10 + "% zusammen!" + " - Insider (;");
+   }
+   liebestest("Airen","Taeke");
